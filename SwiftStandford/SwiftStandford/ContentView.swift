93a4a756d7d47bd6a1fd1e7b6db6c8019b1d8c7a
Nuel <67356270+nuelnuel@users.noreply.github.com>
2023-04-23 09:50:27 -0600

    First upload

---
+++ SwiftStandford/SwiftStandford/ContentView.swift
@@ -0,0 +1,121 @@
+//
+//  ContentView.swift
+//  Pomolife
+//
+//  Created by Ramón Núñez on 20/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var flags: Array = ["🇩🇴", "🇲🇽", "🇹🇷", "🇵🇭", "🇵🇷", "🇧🇳", "🇧🇧", "🇨🇺", "🇺🇸", "🇻🇦", "🇧🇮", "🇰🇲", "🇰🇷", "🇨🇮", "🇰🇵",
+    "🇬🇩", "🇸🇮", "🇱🇾", "🇪🇺"]
+    
+    @State var flagcount: Int = 4
+    
+    var body: some View {
+        
+        VStack {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))] ) {
+                    ForEach(flags[0..<flagcount], id: \.self, content: { flag in
+                        recuadro(content: flag).aspectRatio(2/3, contentMode: .fit)
+                    })
+                }
+            }
+            Spacer();
+            
+            HStack {
+                down
+                Spacer()
+                up
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+        }.padding(.horizontal)
+    }
+    
+    struct recuadro: View {
+        @State var isFaceUp: Bool = true // @State
+        
+        var content: String
+        
+        var body: some View {
+            ZStack {
+                if isFaceUp {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(Color(red: 0.620, green: 0.220, blue: 0.220))
+                } else {
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(Color(red: 1, green: 1, blue: 1))
+                    Text(content).font(.largeTitle)
+                }
+            }.onTapGesture {
+                isFaceUp = !isFaceUp
+            }
+        }
+    }
+    
+    
+    var up: some View {
+        Button(action: {
+            flagcount += 1
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
+        
+    }
+    
+    var down: some   View {
+        Button(action: {
+            flagcount -= 1
+        }, label: {
+            Image(systemName: "minus.circle")
+            
+        })
+    }
+
+    
+    
+    
+    
+    
+    // Views can not be modified.
+    //For temporrary states. Not used that much.
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+}
+
---
+++ SwiftStandford/SwiftStandford/SwiftStandfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStandfordApp.swift
+//  SwiftStandford
+//
+//  Created by Ramón Núñez on 23/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStandfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
