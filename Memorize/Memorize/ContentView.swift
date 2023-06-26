3a44c1007d826c880b6e35aa118c2947be08dfb1
Nuel <67356270+nuelnuel@users.noreply.github.com>
2023-06-25 21:30:28 -0600

    Op

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,20 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
-        VStack {
-            Text("We are going to do this!")
-            HStack{
-                Text("Get me")
-                Text("Ingresa tu nombre")
-            }
+        HStack{
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
-        
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
+
+
+struct CardView: View {
+    var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack{
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(lineWidth: 3)
+                Text("🚗")
+                    .font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 20)
+                    .fill()
+                    .foregroundColor(.red)
+                
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            
     }
 }
