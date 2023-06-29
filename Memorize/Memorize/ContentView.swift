2ae988d7e2e46485409072906573dd96df43d207
Nuel <67356270+nuelnuel@users.noreply.github.com>
2023-06-28 20:19:30 -0600

    CHapter 4

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var emojis: Array = ["😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"]
-    
-    @State var emojiCount = 20
-    
+    @ObservedObject var viewModel: EmojiMemoryGame
+        
     var body: some View {
-        VStack {
             ScrollView{
                 LazyVGrid(columns: [GridItem(.adaptive(minimum:65))]){
-                    ForEach(emojis[0..<emojiCount], id: \.self) {emoji in
-                        CardView(content: emoji).aspectRatio(2/3, contentMode: .fit)
+                    ForEach(viewModel.cards) {card in
+                        CardView(card: card)
+                            .aspectRatio(2/3, contentMode: .fit)
+                            .onTapGesture {
+                                viewModel.choose(card)
+                            }
                     }
                 }
             }
@@ -26,28 +27,23 @@ struct ContentView: View {
             .foregroundColor(.red)
             
             Spacer()
-            
-        }
     }
         
 }
 
-
-
-
 struct CardView: View {
-        var content: String
-        @State var isFaceUp: Bool = true
+        let card: MemoryGame<String>.Card
+    
         var body: some View {
             ZStack{
                 let shape = RoundedRectangle(cornerRadius: 20)
-                if isFaceUp {
+                if card.isFaceUp {
                     shape
                         .fill()
                         .foregroundColor(.black)
                     shape
                         .strokeBorder(lineWidth: 3)
-                    Text(content)
+                    Text(card.content)
                         .font(.largeTitle)
                 } else {
                     shape.fill()
@@ -55,17 +51,14 @@ struct CardView: View {
                         .foregroundColor(.white)
                 }
             }
-            .onTapGesture{
-                isFaceUp = !isFaceUp
-            }
-            
         }
     }
 
 
 struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
-            ContentView()
+            let game = EmojiMemoryGame()
+            ContentView(viewModel: game)
         }
     }
     
--- Memorize/Memorize/EmojiMemoryGame.swift
+++ Memorize/Memorize/EmojiMemoryGame.swift
@@ -6,26 +6,38 @@
 //
 
 
+import SwiftUI
 
-class EmojiMemoryGame {
+
+class EmojiMemoryGame: ObservableObject {
     
-    static let emojis: Array = ["😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙","👌","❤","😍","😉","😓","😳","💪","💩","🍸","🔑","💖","🌟","🎉","🌺","🎶","👠","🏈","⚾","🏆","👽","💀","🐵","🐮","🐩","🐎","💣","👃","👂","🍓","💘","💜","👊","💋","😘","😜","😵","🙏","👋","🚽","💃","💎","🚀","🌙","🎁","⛄","🌊","⛵","🏀","🎱","💰","👶","👸","🐰","🐷","🐍","🐫","🔫","👄","🚲","🍉","💛","💚"]
+    static let emojis = ["😂","😝","😁","😱","👉","🙌","🍻","🔥","🌈","☀","🎈","🌹","💄","🎀","⚽","🎾","🏁","😡","👿","🐻","🐶","🐬","🐟","🍀","👀","🚗","🍎","💝","💙"]
     
     
     static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
-            EmojiMemoryGame.emojis[pairIndex]
+        MemoryGame<String>(numberOfPairsOfCards: 3) { pairIndex in
+            emojis[pairIndex]
             
         }
     }
     
-    private var model: MemoryGame<String> = EmojiMemoryGame.createMemoryGame()
+    
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     var cards: Array<MemoryGame<String>.Card> {
-        return model.cards
+         model.cards
+    }
+    
+    // MARK: –Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card){
+        model.choose(card)
     }
 }
 
 
+    
+
 
 //Be an intermediary between de model and view
+// MVVM ARQUITECTURE
