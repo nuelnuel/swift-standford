191ad1ee2b363e4d7fcbad23c80bff17c1a2246c
Nuel <67356270+nuelnuel@users.noreply.github.com>
2023-07-03 21:47:51 -0600

    z

--- Memorize/Memorize/EmojiMemoryGameView.swift
+++ Memorize/Memorize/EmojiMemoryGameView.swift
@@ -13,7 +13,7 @@ struct EmojiMemoryGameView: View {
         
     var body: some View {
             ScrollView{
-                LazyVGrid(columns: [GridItem(.adaptive(minimum:65))]){
+                LazyVGrid(columns: [GridItem(.adaptive(minimum:100))]){
                     ForEach(game.cards) {card in
                         CardView(card: card)
                             .aspectRatio(2/3, contentMode: .fit)
@@ -34,29 +34,43 @@ struct CardView: View {
         let card: EmojiMemoryGame.Card
     
         var body: some View {
-            ZStack{
-                let shape = RoundedRectangle(cornerRadius: 20)
-                if card.isFaceUp {
-                    shape
-                        .fill()
-                        .foregroundColor(.black)
-                    shape
-                        .strokeBorder(lineWidth: 3)
-                    Text(card.content)
-                        .font(.largeTitle)
-                } else if card.isMatched {
-                    shape.opacity(0)
-                } else {
-                    shape.fill()
-                    Text("?")
-                        .foregroundColor(.white)
+            GeometryReader(content: { geometry in
+                ZStack{
+                    let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+                    if card.isFaceUp {
+                        shape
+                            .fill()
+                            .foregroundColor(.black)
+                        shape
+                            .strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                        Text(card.content)
+                            .font(font(in: geometry.size))
+                    } else if card.isMatched {
+                        shape.opacity(0)
+                    } else {
+                        shape.fill()
+                        Text("?")
+                            .foregroundColor(.white)
+                    }
                 }
+            })
+
             }
-        }
+    
+    private func font(in size: CGSize) -> Font {
+        Font.system(size: min(size.width, size.height) * DrawingConstants.fontScale)
     }
 
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 20
+        static let lineWidth: CGFloat = 3
+        static let fontScale: CGFloat = 0.8
+    }
+        }
 
-struct ContentView_Previews: PreviewProvider {
+
+
+    struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             let game = EmojiMemoryGame()
             EmojiMemoryGameView(game: game)
